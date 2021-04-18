d04f112539faff2cccc4cae61426a00305d5db93
Karlo Bozic <57530033+karlo-bozic@users.noreply.github.com>
2021-04-18 20:55:23 +0100

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,31 +1,62 @@
+/*
+This class connects the SQL database with the eclipse
+It sends query to the database and also displays the results in the table
+*/
+
 package jdbcdemo;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 
 public class Driver {
-	static void display(String QUERY) {
 
+	static ArrayList<Covid> display(String QUERY) {
+
+		ArrayList<Covid> covidList = new ArrayList<>();
+		
 		try {
 			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/oop", "root", "root");
 				
 			Statement myStmt = myConn.createStatement();
-				
+
 			ResultSet myRs = myStmt.executeQuery(QUERY);
-				
 			
+			System.out.println("OBJECTID, County Name, Time Stamp, Confirmed Covid Cases, Population Proportion Covid Cases ");
+						
 			while (myRs.next()) {
+				Covid covid = new Covid(myRs.getInt("OBJECTID"), myRs.getInt("ORIGID"), myRs.getString("TimeStamp"), myRs.getInt("ConfirmedCovidCases"), myRs.getInt("PopulationProportionCovidCases"));
+				covidList.add(covid);
 				
-				System.out.println(myRs.getString("OBJECTID") + ", " + myRs.getString("CountyName") + ", " + myRs.getString("TimeStamp")  + ", " + myRs.getString("ConfirmedCovidCases") + ", " + myRs.getString("PopulationProportionCovidCases"));
-				//System.out.println(myRs.getString("ORIGID") + ", " + myRs.getString("CountyName"));
-			}
+				System.out.println(myRs.getString("OBJECTID") + ", " + myRs.getString("ORIGID") + ", " + myRs.getString("TimeStamp")  + ", " + myRs.getString("ConfirmedCovidCases") + ", " + myRs.getString("PopulationProportionCovidCases"));
+			}			
+			
 		}
 		catch (Exception exc) {
 			JOptionPane.showMessageDialog(null, "Error, please retry");
 			exc.printStackTrace();
 		}
+		return covidList;
 	}
+	
+	public void show() {
+		ArrayList<Covid> list = display();
+		DefaultTableModel model = (DefaultTableModel)table.getModel();
+		Object[] row = new Object[5];
+		
+		for(int i=0;i<list.size();i++) {
+			row[0]=list.get(i).getOBJECTID();
+			row[1]=list.get(i).getCountyName();
+			row[2]=list.get(i).getTimeStamp();
+			row[3]=list.get(i).getConfirmedCovidCases();
+			row[4]=list.get(i).getPopulationProportionCovidCases();
+			model.addRow(row);
+		}
+	}
+
 }
 
 
--- GUI.java
+++ GUI.java
@@ -1,3 +1,9 @@
+/*
+This class creates the GUI for the assignment
+It performs the actions depending on what the users do/input
+
+*/
+
 package jdbcdemo;
 
 import java.awt.FlowLayout;
@@ -5,6 +11,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -12,7 +23,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class GUI extends JFrame implements ActionListener, MouseListener {
 	
@@ -21,13 +34,13 @@ public class GUI extends JFrame implements ActionListener, MouseListener {
 	JTextField text1, text2;
 	JLabel label, label2, label3;
 	JComboBox<String> comboBox;
+	JTable table;
 	//static JLabel label2;
 	
     String[] counties = {"Select All", "Carlow", "Cavan", "Clare", "Cork", "Donegal", "Dublin", "Galway", "Kerry", "Kildare", "Kilkenny",
     		"Laois", "Leitrim", "Limerick", "Longford", "Louth", "Mayo", "Meath", "Monaghan", "Offaly", "Roscommon", "Sligo",
     		"Tipperary", "Waterford", "Westmeath", "Wexford", "Wicklow"};
-
-	
+    	
 	GUI (String title)
 	{
 		super(title);
@@ -49,7 +62,8 @@ public class GUI extends JFrame implements ActionListener, MouseListener {
 		
 		comboBox = new JComboBox<String>(counties);
 		
-
+		table = new JTable(1,5);
+		
 		button1.addActionListener(this);
 		button2.addActionListener(this);
 		button3.addActionListener(this);
@@ -70,9 +84,10 @@ public class GUI extends JFrame implements ActionListener, MouseListener {
 		mid2.add(text1);
 		mid2.add(button2);
 		add(mid2);
-			
+
 		bot1.add(label2);
 		add(bot1);
+		mid2.add(table);
 		bot2.add(text2);
 		bot2.add(button3);
 		add(bot2);
@@ -85,16 +100,18 @@ public class GUI extends JFrame implements ActionListener, MouseListener {
 	{
 		if (e.getSource() == button1)
 		{
-			Driver.display("SELECT * FROM covid INNER JOIN counties ON covid.ORIGID=counties.ORIGID ORDER BY OBJECTID");
+			//Driver.display("SELECT * FROM covid INNER JOIN counties ON covid.ORIGID=counties.ORIGID ORDER BY OBJECTID");
+			Driver.display("SELECT * FROM covid");
 		}
 		else if (e.getSource() == button2)
 		{
-	        String county=(String)comboBox.getSelectedItem();
+	        String county = (String)comboBox.getSelectedItem();
 	        String date = text1.getText();
 
 	        if (county == "Select All"  && date.trim().equals(""))
 	        {
-				Driver.display("SELECT * FROM covid INNER JOIN counties ON covid.ORIGID=counties.ORIGID ORDER BY OBJECTID");		        JOptionPane.showMessageDialog(this, "Select All with no date");
+				Driver.display("SELECT * FROM covid INNER JOIN counties ON covid.ORIGID=counties.ORIGID ORDER BY OBJECTID");
+				JOptionPane.showMessageDialog(this, "Select All with no date");
 	        }
 	        else if(county == "Select All")
 	        {
@@ -109,7 +126,7 @@ public class GUI extends JFrame implements ActionListener, MouseListener {
 	        }
 	        else
 	        {
-		        Driver.display("SELECT * FROM covid INNER JOIN counties ON covid.ORIGID=counties.ORIGID WHERE counties.CountyName='"+county+"' ORDER BY OBJECTID");
+		        Driver.display("SELECT * FROM covid INNER JOIN counties ON covid.ORIGID=counties.ORIGID WHERE counties.CountyName='"+county+"' AND covid.TimeStamp='"+date+"' ORDER BY OBJECTID");
 	        }
 		}
 		else if (e.getSource() == button3)
@@ -127,4 +144,40 @@ public class GUI extends JFrame implements ActionListener, MouseListener {
 		}
 	}
 
+/*
+	
+	public ArrayList<Covid> List() {
+		ArrayList<Covid> covidList = new ArrayList<>();
+		
+		try {
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/oop", "root", "root");
+				
+			Statement myStmt = myConn.createStatement();
+			String QUERY = "Select * FROM covidtest";
+			ResultSet myRs = myStmt.executeQuery(QUERY);
+			while(myRs.next()) {
+				Covid covid = new Covid(myRs.getInt("OBJECTID"), myRs.getInt("ORIGID"), myRs.getString("TimeStamp"), myRs.getInt("ConfirmedCovidCases"), myRs.getInt("PopulationProportionCovidCases"));
+				covidList.add(covid);	
+			}
+			
+		}
+		catch (Exception exc) {
+			JOptionPane.showMessageDialog(null, "Error, please retry");
+			exc.printStackTrace();
+			}
+			return covidList;
+		}
+	
+
+	public void show() {
+		ArrayList<Covid> list = List();
+		DefaultTableModel model = (DefaultTableModel)table.getModel();
+		model.setRowCount(0);
+		
+		for (Covid covid : list) {
+            Object rowData[] = {covid.getOBJECTID(), covid.getORIGID(), covid.getTimeStamp(), covid.getConfirmedCovidCases(), covid.getPopulationProportionCovidCases()};
+            model.addRow(rowData);
+		}
+	}
+*/
 }
